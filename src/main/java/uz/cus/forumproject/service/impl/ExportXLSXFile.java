ca050fd0b073435a251cf28391b077a4e94318a5
eeslambec <eeslambec@gmail.com>
2024-05-17 19:11:51 +0500

    shuffling completed

--- src/main/java/uz/cus/forumproject/service/impl/ExportXLSXFile.java
+++ src/main/java/uz/cus/forumproject/service/impl/ExportXLSXFile.java
@@ -14,6 +14,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -27,6 +28,7 @@ public class ExportXLSXFile {
 
     public String exportXLSXFile() throws IOException {
         List<Form> forms = formService.getAll();
+        Collections.shuffle(forms);
         for (int i = 0; i < columns.length; i++) {
             Cell cell = headerRow.createCell(i);
             cell.setCellValue(columns[i]);
